8a548f4e99214fbfbafb817f14577827558cc0e9
sanket-hasher <163403307+sanket-hasher@users.noreply.github.com>
2024-04-26 00:30:21 +0530

    Unesco.py

--- Unesco.py
+++ Unesco.py
@@ -1,6 +1,8 @@
-
+import time
 class UNESCO:
     def __init__(self):
+        print(" WELCOME TO UNESCO.org ")
+        print("-------------------------------")
         self.heritage_sites = []
 
     def login(self, user, password):
@@ -19,7 +21,7 @@ class UNESCO:
         name = input(" Enter the name of the site ")
         state = input("Enter the State ")
         district = input('Enter the district ')
-        year = int(input("Enter the year when {name} was established ".format(name=name)))
+        year = input("Enter the year when {name} was established ".format(name=name))
         reason = input(" Why it was established ")
         website = input("Enter the website link of the {} ".format(name))
         details = dict()
@@ -36,21 +38,27 @@ class UNESCO:
     def display(self):
         for sites in self.heritage_sites:
             for key, value in sites.items():
-                print("{}  -  {}".format(key, value))
+                print("{}  -  {}".format(key.upper(), value.upper()))
+            print('--------------------------------------------')
 
     def display_state(self, state):
+        found=[]
         for sites in self.heritage_sites:
-            for key, value in sites.items():
-                if (state in value):
-                    print("Name of the Heritage - ", sites['Name'])
-                    return
-        print("State or district not found")
+            
+            if (state in sites.values()):
+                found.append(sites['Name'])
+        if(found==[]):    
+            print("State  not found")
+        else:
+            print('SITES FOUND in ',state )
+            print(' , '.join(found))
+        
 
     def display_site(self, site):
         for sites in self.heritage_sites:
             if (site == sites['Name']):
                 for key, value in sites.items():
-                    print("{0} - {1}".format(key, value))
+                    print("{0} - {1}".format(key.upper(), value.upper()))
                 return
 
         print(" HERITAGE SITE NOT FOUND ")
@@ -65,29 +73,28 @@ class UNESCO:
 
 h = UNESCO()
 attempts=3
-print("Enter details 1")
-h.add_sites()
-print("Enter details 2")
-h.add_sites()
-"""
-print("Enter details 3")
-h.add_sites()"""
-h.display()
-session=2
-for i in range(1,10):
+print("Enter the username and password correctly 3 wrong attempts will lead to account block ")
+
+session=99
+for i in range(1,session):
+    c=input("Enter 'c' to continue and 'end' to stop ")
+    
+    if(c=='end'):
+        print('Session interrupted')
+        break
     if session==0:
         break
     user = input(" Enter the username ")
     password = input(" Enter the password ")
     if (h.login(user, password) == 1):
-        session-=1
+        
         print(" You Are ADMIN ")
         print()
         command = input(" Enter 'a' to add and 's' to display and 'd' to delete site ")
         if (command == 'a'):
 
             while True:
-                stp = input(" Enter 'Stop' to stop adding ")
+                stp = input(" Enter 'Stop' to stop adding   PRESS ANY KEY TO CONTINUE ")
                 if (stp == 'Stop'):
                     print("YOU CANNOT ENTER ANY MORE")
                     h.display()
@@ -105,20 +112,20 @@ for i in range(1,10):
         elif (command == 'd'):
             site = input(" Enter the site to delete ")
             h.delete_site(site)
+            h.display()
         else:
             print(" Invalid Operation ")
     elif (h.login(user, password) == 2):
-        session-=1
+        
         print(" You Are VISITOR ")
         print()
         command = input(" Enter s to display by search and d to display all ")
         if (command == 's'):
-            search_type = input(
-                "Enter search type 's-site' to search by site or 'view-site' to see location of a site ")
-            if (search_type == 's-site'):
+            search_type = input("Enter search type 'v' to search by site or 'l' to see sites in the location ")
+            if (search_type == 'v'):
                 site = input(" Enter the site to display ")
                 h.display_site(site)
-            elif (search_type == 'view-site'):
+            elif (search_type == 'l'):
                 state = input("Enter the State ")
                 h.display_state(state)
             else:
@@ -132,6 +139,7 @@ for i in range(1,10):
         print("Wrong user id or password Re - login {} attempts left ".format(attempts))
         if attempts==0:
             print("Account blocked ")
-            break
+            time.sleep(60)
+
 
 
